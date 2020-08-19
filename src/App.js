d7f0549e0bde4619561b1e90fd604cc638edba06
zeepk <zeepkdb@gmail.com>
2020-08-18 18:45:04 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import 'primeflex/primeflex.css';
+import { InputText } from 'primereact/inputtext';
+import { Slider } from 'primereact/slider';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div
+				style={{
+					minHeight: '80vh',
+					backgroundColor: 'white',
+					borderRadius: '5%',
+					padding: '50px',
+					margin: '0 auto',
+					border: '3px solid black',
+					boxShadow: '-5px 10px 10px black',
+				}}
+			>
+				<div className="p-field p-grid">
+					<label
+						htmlFor="firstname3"
+						className="p-col-fixed"
+						style={{ width: '100px' }}
+					>
+						Firstname
+					</label>
+					<div className="p-col">
+						<InputText id="firstname3" type="text" />
+					</div>
+					<label
+						htmlFor="lastname3"
+						className="p-col-fixed"
+						style={{ width: '100px' }}
+					>
+						Lastname
+					</label>
+					<div className="p-col">
+						<InputText id="lastname3" type="text" />
+					</div>
+				</div>
+				<div className="p-field p-grid">
+					<Slider
+						value={this.state.value}
+						onChange={(e) => this.setState({ value: e.value })}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
